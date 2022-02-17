57ddc053efea40ee3efd3bf16a1ad241493c711b
Ritish jaiswal <66643595+ritishjaiswal20@users.noreply.github.com>
2022-02-17 18:12:06 +0530

    Create Find middle element in a Linked List.cpp
    https://takeuforward.org/data-structure/find-middle-element-in-a-linked-list/

---
+++ Find middle element in a Linked List.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+}*first;
+void create()
+{
+    node*p;
+    p=new node;
+    int n;
+    cout<<"enter the lenght of the linked list";
+    cin>>n;
+    int x;
+    cin>>x;
+    p->data=x;
+    p->next=0;
+    first=p;
+    for(int i=1;i<n;i++)
+    {
+      cin>>x;
+      node *t;
+      t=new node;
+      t->data=x;
+      t->next=0;
+      p->next=t;
+      p=t;
+    }
+}
+void findmiddle()
+{
+    node* slow=first;
+    node* fast=first;
+    while(slow && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    cout<<"middlle ellement";
+    cout<<slow->data;
+}
+void display(node *first)
+{ 
+    
+    cout<<"hey buddy";
+    node*p= first;
+    while(p)
+    {
+        cout<<p->data;
+        p=p->next;
+    }
+}
+int main()
+{
+        create();
+        cout<<endl;
+        findmiddle();
+    return 0;
+}
