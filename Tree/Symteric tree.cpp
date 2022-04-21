581a9d0ac13445db1859f86d0ef2a5e1982f015b
Ritish jaiswal <66643595+ritishjaiswal20@users.noreply.github.com>
2022-04-21 10:52:41 +0530

    Create Symteric tree.cpp
    https://github.com/ritishjaiswal20/DSA-Sheet-solution/new/main/Tree

---
+++ Tree/Symteric tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ism(TreeNode* l,TreeNode *r )
+    {
+      if(l==NULL && r==NULL) return true;
+      if(l==NULL && r!=NULL) return false;
+      if(l!=NULL && r==NULL) return false;
+      if(l->val!=r->val) return false;
+      return (ism(l->left,r->right) && ism(l->right,r->left));
+      
+    }
+    bool isSymmetric(TreeNode* root) {
+     return ism(root->left ,root->right); 
+
+    }
+};
