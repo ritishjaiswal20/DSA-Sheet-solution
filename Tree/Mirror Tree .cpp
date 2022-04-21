a97b9319d17f78bb09af21388f4554cea7ad6a4b
Ritish jaiswal <66643595+ritishjaiswal20@users.noreply.github.com>
2022-04-21 15:16:00 +0530

    Create Mirror Tree .cpp
    https://practice.geeksforgeeks.org/problems/mirror-tree/1

---
+++ Tree/Mirror Tree .cpp
@@ -0,0 +1,35 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+          // code here
+    if(!node)
+    {
+        return ;
+    }
+    mirror(node->left);
+    mirror(node->right);
+    
+    Node* t;
+    t=node->left;
+    node->left=node->right;
+    node->right=t;
+    
+    return;
+    
+    }
+};
