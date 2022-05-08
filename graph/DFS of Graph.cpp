54cb45148410c8565ffa6f047e2eb70f57452a3a
Ritish jaiswal <66643595+ritishjaiswal20@users.noreply.github.com>
2022-05-08 18:44:02 +0530

    Create DFS of Graph.cpp
    https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1

---
+++ graph/DFS of Graph.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+      void df(int i,vector<int>adj[],vector<int>&storedfs,vector<int>&vis)
+     {
+          storedfs.push_back(i);
+          vis[i]=1;
+          for(auto it:adj[i])
+          {
+              if(!vis[it])
+              {
+                  df(it,adj,storedfs,vis);
+              }
+          }
+         
+     }
+    vector<int> dfsOfGraph(int v, vector<int> adj[]https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1) {
+        // Code her   
+        vector<int>storedfs;
+        vector<int>vis(v,0);
+        df(0,adj,storedfs,vis);
+        return storedfs;
+    }
+};
