fa8eafda010662c562c8bd94a7852180092cfebb
Ritish jaiswal <66643595+ritishjaiswal20@users.noreply.github.com>
2022-07-20 01:43:15 +0530

    Update Floor in BST.cpp

--- BST/Floor in BST.cpp
+++ BST/Floor in BST.cpp
@@ -1,43 +1,25 @@
-/************************************************************
-
-    Following is the TreeNode class structure
-
-    template <typename T>
-    class TreeNode {
-       public:
-        T val;
-        TreeNode<T> *left;
-        TreeNode<T> *right;
-
-        TreeNode(T val) {
-            this->val = val;
-            left = NULL;
-            right = NULL;
-        }
-    };
-
-************************************************************/
-
-int flooor(TreeNode<int>* root,int fmax,int key)
-{
-	if(root==NULL)
-	{
-		return fmax;
-	}
-	if(root->val ==key){
-		return root->val;
-	}
-	if(root->val>key){
-		return flooor(root->left,fmax,key);
-	}
-	else{
-		fmax=root->val;
-		return flooor(root->right,fmax,key);
-	}
-	return fmax;
-}
-int floorInBST(TreeNode<int> * root, int X)
+int floorInBST(TreeNode<int> * p, int x)
 {
     // Write your code here.
-    return flooor(root,-1,X);
+    int pre=-1;
+    while(p)
+    {
+        if(p->val==x)
+        {
+            return x;
+   
+        }
+        else {
+        if(p->val<x)
+        {
+            pre=p->val;
+            p=p->right;
+        }
+        else
+        {
+            p=p->left;
+        }
+        }
+    }
+    return pre;
 }
